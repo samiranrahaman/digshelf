785a0bce15b89676d3c419fffee1633ec065d593
samiran <samiran.r@medleymed.com>
2019-11-19 17:06:08 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
 import logo from './logo.svg';
 import './App.css';
 import Product from './components/product/product';
@@ -6,13 +10,24 @@ import Product from './components/product/product';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <Product></Product>
+      <Router>
+      <div>
+        <h2>Welcome to React Router Tutorial</h2>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <ul className="navbar-nav mr-auto">
+          <li><Link to={'/'} className="nav-link"> Home </Link></li>
+          <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+          <li><Link to={'/about'} className="nav-link">About</Link></li>
+        </ul>
+        </nav>
+        <hr />
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/contact' component={Contact} />
+            <Route path='/about' component={About} />
+        </Switch>
       </div>
+    </Router>
     );
   }
 }
